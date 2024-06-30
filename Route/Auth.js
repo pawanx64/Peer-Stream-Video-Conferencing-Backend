f84f1f9aab2f99370b642fcb844e6f860627970c
unknown <pawankumar.nov5@gmail.com>
2024-06-30 22:13:39 +0530

    commit

--- Route/Auth.js
+++ Route/Auth.js
@@ -2,8 +2,8 @@ const express=require('express');
 const router=express.Router();
 const Auth=require('../Controller/Auth')
 
-router.post('/register',Auth.register);
 router.post('/login',Auth.login);
+router.post('/register',Auth.register);
 router.get('/user',Auth.getUser);
 
 module.exports=router;
